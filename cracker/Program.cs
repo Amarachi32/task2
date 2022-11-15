daa0abceb7df3f6e5156130d055b5a55c366397f
IZUEGBUNAM PERPETUA AMARACHI <54138835+Amarachi32@users.noreply.github.com>
2022-11-15 11:10:09 +0100

    Add files via upload

---
+++ cracker/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class CodeCracker
+{
+   static void Main()
+   {
+    string key = "$*%&><!)#(@abcdefghijklmno";
+    string plainText = "welcome to bezao cohort three";
+    string cypherText = Encrypt(plainText, key);
+    string decryptedText = Decrypt(cypherText, key);
+
+      Console.WriteLine("Plain     : {0}", plainText);
+      Console.WriteLine("Encrypted : {0}", cypherText);
+      Console.WriteLine("Decrypted : {0}", plainText);
+      Console.ReadKey();
+   }
+
+   static string Encrypt(string plainText, string key)
+   {
+      char[] chars = new char[plainText.Length];
+
+      for(int i = 0; i < plainText.Length; i++)
+      {
+         if (plainText[i] == ' ')
+         {
+            chars[i] = ' ';
+         }  
+         else
+         {
+            int j = plainText[i] - 97;
+            chars[i] = key[j];
+            // Console.WriteLine(chars[i]);
+         }
+      }
+
+      return new string(chars);
+   }
+
+   static string Decrypt(string cypherText, string key)
+   {
+      char[] chars = new char[cypherText.Length];
+
+      for(int i = 0; i < cypherText.Length; i++)
+      {
+         if (cypherText[i] == ' ')
+         {
+            chars[i] = ' ';
+         }  
+         else
+         {
+            int j = key.IndexOf(cypherText[i]) - 97;
+            chars[i] = (char)j;
+         }
+      }
+
+      return new string(chars);
+   }
+}  
+
